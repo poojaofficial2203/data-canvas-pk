bd85cdd753ca108a2efafd9bc34ad38d7fdac088
poojaofficial2203 <poojaofficial2203@gmail.com>
2026-06-11 16:05:08 +0530

    Update __init__.py

--- data_canvas/__init__.py
+++ data_canvas/__init__.py
@@ -1,34 +1,5 @@
-"""Data Canvas - Universal Data to PowerPoint Converter Package"""
-
 from .universal_converter import UniversalDataToPPT
-#from .performance import PerformanceOptimizer, ChunkProcessor, CacheManager
-#from .summarizer import DataSummarizer, StructuredReportGenerator, PerformanceSummary, DataSummary
 from .analyzers import DataAnalyzer
 from .generators import ChartGenerator
-#from .utils import (
-    parse_raw_data,
-    load_data,
-    get_numeric_columns,
-    get_categorical_columns,
-    detect_delimiter
-)
 
 __version__ = "2.0.0"
-__author__ = "Data Canvas Team"
-__all__ = [
-    "UniversalDataToPPT",
-    "PerformanceOptimizer",
-    "ChunkProcessor",
-    "CacheManager",
-    "DataSummarizer",
-    "StructuredReportGenerator",
-    "PerformanceSummary",
-    "DataSummary",
-    "DataAnalyzer",
-    "ChartGenerator",
-    "parse_raw_data",
-    "load_data",
-    "get_numeric_columns",
-    "get_categorical_columns",
-    "detect_delimiter",
-]
